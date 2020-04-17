91954ff66ccb52c450477e6ab296d7f56cbee609
Sud Rattu <Sudesh@sudeshs-mbp.home>
2020-04-17 19:13:22 +0100

    Amended list snapshots to only show latest snapshot

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,4 +1,5 @@
 import boto3
+import botocore
 import click
 
 session = boto3.Session(profile_name='shotty')
@@ -43,6 +44,9 @@ def list_snapshots(project):
                     s.progress,
                     s.start_time.strftime("%c")
                 )))
+                
+                if s.state =='completed': 
+                    break
        
     return
     
@@ -88,9 +92,19 @@ def create_snapshots(project):
     instances = filter_instances(project)
     
     for i in instances:
+        i.stop()
+        i.wait_until_stopped()
+        
         for v in i.volumes.all():
             print("Creating snapshot of {0}".format(v.id))
-            v.create_snapshot(Descrtiption="Created by Shotty")
+            v.create_snapshot(Description="Created by Shotty")
+        
+        print("Starting {0}...".format(i.id))
+        
+        i.start()
+        i.wait_until_running()
+    
+    print("Jobs done!")
     
     return
                  
@@ -128,7 +142,11 @@ def stop_instances(project):
     
     for i in instances:
         print("Stopping {0}...".format(i.id))
-        i.stop()
+        try:
+            i.stop()
+        except botocore.exceptions.ClientError as e:
+            print("Could not stop {0}. ".format(i.id) + str(e))
+            continue
         
     return
 
@@ -143,8 +161,11 @@ def start_instances(project):
     
     for i in instances:
         print("Starting {0}...".format(i.id))
-        i.start()
-        
+        try:
+            i.start()
+        except botocore.exceptions.ClientError as e:
+            print("Could not start {0}. ".format(i.id) + str(e))
+            continue        
     return
 
 if __name__ =='__main__':
